b971793e44c593a72795f48ba9bc209b0892cb0e
loanwolf0 <anurag12775@gmail.com>
2024-07-01 23:56:50 +0530

    unknow

---
+++ client/src/components/About/Product.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import about1 from "../../assests/About/about1.png";
+
+const Product = () => {
+  return <div>product</div>;
+};
+
+export default Product;
---
+++ client/src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "../../components/About/Product";
+
+const About = () => {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+};
+
+export default About;
